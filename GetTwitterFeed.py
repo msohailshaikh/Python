45cf27ba0724227b9e7a8c91e3bbd423bba31120
msohailshaikh <msohailshaikh@users.noreply.github.com>
2018-02-20 10:28:27 +0500

    Add files via upload

---
+++ GetTwitterFeed.py
@@ -0,0 +1,27 @@
+# Python:       Program for Mining Twitter Feed
+# Author:       Sohail Shaikh
+# Created:      19-Feb-2018
+# Modified:     19-Feb-2018
+# Script:       getTwitter-Feed.py
+
+import tweepy
+from tweepy import OAuthHandler
+import json
+
+file = open('twitoken.json', 'r')
+ds = json.load(file)
+
+consumer_key = ds['consumer_key']
+consumer_secret = ds['consumer_secret']
+access_token = ds['access_token_key']
+access_secret = ds['access_token_secret']
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+api.verify_credentials()
+s = tweepy.Cursor(api.home_timeline).items(6)
+
+for status in s:
+    print(status.created_at, status.text)
